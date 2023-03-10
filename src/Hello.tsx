d3ab043f2be5decc162bec18160f80c9415b072b
Freewind <nowindlee@gmail.com>
2023-03-10 18:26:01 +0800

    typescript-vite-popper-react-demo

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,25 @@
-import React, { FC } from 'react';
-import './Hello.pcss';
+import React, { FC, useState } from 'react';
+import { usePopper } from 'react-popper';
+import './index.css';
 
 type Props = {};
 
-export const Hello: FC<Props> = ({}) => {
-  return <div className={'Hello'}>
-    <h1>Hello React</h1>
-  </div>;
+export const Hello: FC<Props> = ({ }) => {
+  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLElement | null>(null);
+  const [arrowElement, setArrowElement] = useState<HTMLElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+  });
+
+  return (
+    <>
+      <div id="popcorn" ref={setReferenceElement} />
+
+      <div id="tooltip" ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+        Popper element
+        <div id="arrow" ref={setArrowElement} style={styles.arrow} />
+      </div>
+    </>
+  );
 }
